212210c2bbb0017623ff7957dedc5ed34ca38dd8
Elaine <elainefelipemoreira@gmail.com>
2022-02-15 19:40:47 -0300

    começa a implementar metodo POST

--- index.js
+++ index.js
@@ -25,12 +25,29 @@ app.get('/books', (req, res) => {
 
 // });
 //============adiciona um novo livro================//
-app.post('/', (req, res) => {
-
+app.post('/books', (req, res) => {
+  // extrair informacoes do req.body
+  const { title, author } = req.body;
+  // calcular o novo id do livro que vai ser adicionado
+  // pega o ultimo livro
+  //doa array de livros eu pego o index igual ao tamanho total menos 1, desse ultimo livro pego id e faço mais
+  //id do ultimo livro + 1
+  const newId = books[books.length - 1].id + 1;
+  // cria uma constante com os dados dos livros
+  const newBook = {
+    id: newId,
+    title,
+    author
+  };
+  // colocando os dados do novo livro dentro do array
+  books.push(newBook);
+  // vamos dar um status 201, que quer dizer que o livro foi criado
+  res.status(201).json(newBook);
 });
 //============atualiza os livros================//
 app.put('/', (req, res) => {
-
+  // extrair dados da request parametros de rota
+  // req.params contem a chave id
 });
 //============apaga livros================//
 app.delete('/', (req, res) => {
