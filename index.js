384bcad50c944b056e3dfe7f80182c13f0f91d25
Elaine <elainefelipemoreira@gmail.com>
2022-02-16 19:09:53 -0300

    modifica importacao no index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const books = require('./books.json');
 const getBooks = require('./middlewares/getBooks');
 const postBooks = require('./middlewares/postBooks');
 const putBooks = require('./middlewares/putBooks');
