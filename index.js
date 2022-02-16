5210316ebd92c52f208387556375d8c715342e54
Elaine <elainefelipemoreira@gmail.com>
2022-02-16 17:26:23 -0300

    troca nome de livros

--- index.js
+++ index.js
@@ -7,13 +7,40 @@ const PORT = 3000;
 app.use(bodyParser.json());
 //==========banco de dados==========//
 const books = [
-  { id: 1, title: 'The Lord of Rings', author: 'J.R.R. Tolkien' },
-  { id: 2, title: 'Dune', author: 'Frank Herbert' },
-  { id: 3, title: 'Foundation', author: 'Isaac Asimov' },
-  { id: 4, title: 'The Man in The High Castle', author: 'Philip K Dick' },
-  { id: 5, title: '1984', author: 'George Orwell' },
-  { id: 6, title: 'Brave New World', author: 'Aldous Huxley' },
-  { id: 7, title: '7 minutos depois da meia noite', author: 'Patrick Ness' },
+  { id: 1, 
+    title: 'Peça-me o que quiser', 
+    author: 'megan maxwell' 
+  },
+  { 
+    id: 2, 
+    title: 'Dune', 
+    author: 'megan maxwell' 
+  },
+  { 
+    id: 3, 
+    title: 'O Terapeuta', 
+    author: 'valentina k michael' 
+  },
+  { 
+    id: 4, 
+    title: 'Admirador Secreto', 
+    author: 'valentina k michael' 
+  },
+  { 
+    id: 5, 
+    title: 'O Juiz', 
+    author: 'Katherine Laccomt' 
+  },
+  { 
+    id: 6, 
+    title: 'Veredito Final', 
+    author: 'Katherine Laccomt' 
+  },
+  { 
+    id: 7, 
+    title: 'Eu Sou Eric Zimmerman', 
+    author: 'megan maxwell' 
+  },
 ];
 //=============================pega todos os livros=====================================//
 app.get('/books', (req, res) => {
