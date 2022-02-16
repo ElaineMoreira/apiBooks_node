53ce8f1bb8a4ee12014937e20f3e884b6e436111
Elaine <elainefelipemoreira@gmail.com>
2022-02-16 16:53:09 -0300

    Implemeta metodo PUT

--- index.js
+++ index.js
@@ -15,16 +15,16 @@ const books = [
   { id: 6, title: 'Brave New World', author: 'Aldous Huxley' },
   { id: 7, title: '7 minutos depois da meia noite', author: 'Patrick Ness' },
 ];
-//===============pega todos os livros===================//
+//=============================pega todos os livros=====================================//
 app.get('/books', (req, res) => {
   // usar json para retorna a resposta em json
   res.status(200).json({ books });
 });
-//============pega todos os livros po id================//
+//============================pega todos os livros po id================================//
 // app.get('/', (req, res) => {
 
 // });
-//============adiciona um novo livro================//
+//===========================adiciona um novo livro=====================================//
 app.post('/books', (req, res) => {
   // extrair informacoes do req.body
   const { title, author } = req.body;
@@ -48,12 +48,29 @@ app.post('/books', (req, res) => {
   // vamos dar um status 201, que quer dizer que o livro foi criado
   res.status(201).json(newBook);
 });
-//============atualiza os livros================//
-app.put('/', (req, res) => {
+//=================================atualiza os livros==================================//
+app.put('/books/:id', (req, res) => {
   // extrair dados da request parametros de rota
   // req.params contem a chave id
+  const { id } = req.params;
+  // vamos pegar o titulo e o autor do body
+  const { title, author } = req.body;
+// para ver em qual posicao do array ele esta
+// findIndex-retorna a posicao daquele item no array
+// ${book.id}` === id = compara string com numero
+  const bookIndex = books.findIndex(book => `${book.id}` === id)
+  const book = books[bookIndex];
+// se nao achar o livro if/else
+  if (!book || !bookIndex) {
+    return res.status(404).json({ message: `livro com o id ${id}, não existe` })
+  }
+  // se achar o livro retorna
+  book.title = title;
+  book.author = author;
+  books[bookIndex] = book;
+  res.status(200).json(book);
 });
-//============apaga livros================//
+//====================================apaga livros=====================================//
 app.delete('/', (req, res) => {
 
 });
