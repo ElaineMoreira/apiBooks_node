5f3c12069b5e0711fab1139dcecc2173e091210e
Elaine <elainefelipemoreira@gmail.com>
2022-02-16 17:05:18 -0300

    implementa metodo DELETE

--- index.js
+++ index.js
@@ -71,8 +71,18 @@ app.put('/books/:id', (req, res) => {
   res.status(200).json(book);
 });
 //====================================apaga livros=====================================//
-app.delete('/', (req, res) => {
-
+app.delete('/books/:id', (req, res) => {
+  const { id } = req.params;
+  const bookIndex = books.findIndex(book => `${book.id}` === id);
+  // caso especifico do delete que e se eu nao achei eu nao preciso dar erro
+  // caso nao exista o livro
+  if (!bookIndex === -1) {
+    res.status(204).json.end();
+  }
+  // se o livro existir
+  // usa o splice, comando de remover
+  books.splice(bookIndex, 1);
+  res.status(204).end()
 });
 
 app.listen(PORT, () => console.log(`Online na porta ${PORT}`));
